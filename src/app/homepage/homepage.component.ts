5518d2a5d21d0dc4a3b9d18f321ac3257bd9fb7c
rajatguptaTechlib <rajatinvoice@gmail.com>
2019-08-17 14:28:37 +0530

    Added Toast For Notifications

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastrService } from 'ngx-toastr'; 
 
 @Component({
   selector: 'app-homepage',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private toastr: ToastrService) { }
 
   ngOnInit() {
+  }
+
+  successmsg(){  
+    this.toastr.success("Toastr Success message",'Success')  
+  }
+  errorsmsg(){  
+    this.toastr.error("Toastr Success message",'Success')  
+  }
+  infotoastr(){  
+    this.toastr.info("Toastr Success message",'Success')  
+  }
+  toastrwaring(){  
+    this.toastr.warning("Toastr Success message",'Success')  
   }
 
 }
